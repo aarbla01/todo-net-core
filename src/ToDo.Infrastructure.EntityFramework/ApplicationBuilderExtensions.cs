77ca5a3aafa897f3e0335d34ddeaba50da3bdbf3
Aaron Blake <aaron.blake@aablakeenterprises.com>
2021-03-03 20:53:12 +0000

    Update README

---
+++ src/ToDo.Infrastructure.EntityFramework/ApplicationBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ToDo.Infrastructure.EntityFramework
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static void ApplyDatabaseMigrations(this IApplicationBuilder builder)
+        {
+            using var scope = builder.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope();
+            using var ctx = scope.ServiceProvider.GetRequiredService<ToDoDbContext>();
+            ctx.Database.Migrate();
+        }
+    }
+}
